e0409163e6b95c434de2aaa547b0d395794cf91f
Christoffer Nguyen <christofferhoang@gmail.com>
2022-10-27 17:53:37 +0200

    PROG: rspec COMPLETED

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Clearing DB'
+Movie.destroy_all
+
+puts 'Creating some fake movies'
+20.times do
+  Movie.create(title: Faker::Movie.title, overview: Faker::Lorem.sentence, poster_url: Faker::Avatar.image, rating: rand(0..10))
+end
+
+puts 'Finished!'
+puts "Made #{Movie.count} movies"
