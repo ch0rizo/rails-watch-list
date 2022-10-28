30dbe69e60219ed1cc14ceae7289b05466a213f6
Christoffer Nguyen <christofferhoang@gmail.com>
2022-10-28 16:05:28 +0200

    FEAT: display images

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,12 @@ data = JSON.parse(data_serialized)
 relevant_infos = data["results"]
 
 relevant_infos.each do |info|
-  Movie.create(title: info["original_title"], overview: info["overview"], poster_url: info["poster_path"], rating: info["popularity"])
+  poster_url = "https://image.tmdb.org/t/p/original/#{info["poster_path"]}"
+  file = URI.open(poster_url)
+  movie = Movie.new(title: info["original_title"], overview: info["overview"], poster_url: poster_url, rating: info["popularity"])
+  movie.photo.attach(io: file, filename: "#{info["original_title"].gsub(" ", "-")}.png", content_type: "image/png")
+  movie.save
 end
-
-# Images https://image.tmdb.org/t/p/original/******
 
 puts 'Finished!'
 puts "Made #{Movie.count} movies"
