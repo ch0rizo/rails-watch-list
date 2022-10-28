8e0993aaf95c45e31d99706a04ce79595c8523da
Christoffer Nguyen <christofferhoang@gmail.com>
2022-10-28 12:54:18 +0200

    FEAT: form with image

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -26,6 +26,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id, :photo)
   end
 end
