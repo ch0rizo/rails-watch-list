7657343baf9ed0b5d1c41a8616f3573e108fee89
Christoffer Nguyen <christofferhoang@gmail.com>
2022-10-28 14:54:45 +0200

    FEAT: new movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
+require "json"
+require "open-uri"
+
 puts 'Clearing DB'
+Bookmark.destroy_all
 Movie.destroy_all
 
-puts 'Creating some fake movies'
-20.times do
-  Movie.create(title: Faker::Movie.title, overview: Faker::Lorem.sentence, poster_url: Faker::Avatar.image, rating: rand(0..10))
+url = "https://tmdb.lewagon.com/movie/top_rated"
+data_serialized = URI.open(url).read
+data = JSON.parse(data_serialized)
+
+relevant_infos = data["results"]
+
+relevant_infos.each do |info|
+  Movie.create(title: info["original_title"], overview: info["overview"], poster_url: info["poster_path"], rating: info["popularity"])
 end
+
+# Images https://image.tmdb.org/t/p/original/******
 
 puts 'Finished!'
 puts "Made #{Movie.count} movies"
